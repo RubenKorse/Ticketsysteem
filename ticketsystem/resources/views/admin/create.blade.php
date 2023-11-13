7755b2abbc466b06a9efb31e9716acc3eb662ac0
RubenKorse <121227805+RubenKorse@users.noreply.github.com>
2023-11-13 10:37:49 +0100

    update create page

--- ticketsystem/resources/views/admin/create.blade.php
+++ ticketsystem/resources/views/admin/create.blade.php
@@ -1,43 +1,48 @@
 <x-admin-layout>
-    <div class="flex justify-center items-center h-screen">
-        <div class="bg-white p-8 rounded shadow-lg w-1/3 max-h-full">
-            @if ($errors->any())
-                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
+    <div class="flex justify-center items-center h-screen mt-20">
+        <div class="bg-white p-8 rounded shadow-lg w-1/3 max-h-schreen">
             <form action="{{ route('admin.store') }}" method="POST">
                 @csrf
 
                 <div class="mb-4">
                     <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title:</label>
                     <input type="text" id="title" name="title" class="form-input rounded-md w-full">
+                    @error('title')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description:</label>
                     <input id="description" type="hidden" name="description">
                     <trix-editor input="description"></trix-editor>
+                    @error('description')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="date" class="block text-gray-700 text-sm font-bold mb-2">Date:</label>
                     <input type="date" id="date" name="date" class="form-input rounded-md w-full">
+                    @error('date')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="time" class="block text-gray-700 text-sm font-bold mb-2">Time:</label>
                     <input type="time" id="time" name="time" class="form-input rounded-md w-full">
+                    @error('time')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="location" class="block text-gray-700 text-sm font-bold mb-2">Location:</label>
                     <input type="text" id="location" name="location" class="form-input rounded-md w-full">
+                    @error('location')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
@@ -47,6 +52,17 @@
                         <option value="Festivals">Festivals</option>
                         <option value="Films">Films</option>
                     </select>
+                    @error('category')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="price" class="block text-gray-700 text-sm font-bold mb-2">Price:</label>
+                    <input type="number" step="0.01" id="price" name="price" class="form-input rounded-md w-full" placeholder="Enter the price">
+                    @error('price')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="flex justify-center mt-6">
