daaea67e28afdd95922591692e86d62ce5af80ef
RubenKorse <121227805+RubenKorse@users.noreply.github.com>
2023-11-14 15:05:32 +0100

    functions for difrent categorys

--- ticketsystem/app/Http/Controllers/EventController.php
+++ ticketsystem/app/Http/Controllers/EventController.php
@@ -16,4 +16,31 @@ class EventController extends Controller
 
         return view('index', compact('events'));
     }
+
+    public function sports(){
+        $events = Event::where('category', 'sports')
+                       ->whereDate('date', '>=', Carbon::tomorrow())
+                       ->orderBy('date', 'asc')
+                       ->get();
+
+        return view('sports', compact('events'));
+    }
+
+    public function festivals(){
+        $events = Event::where('category', 'festivals')
+                       ->whereDate('date', '>=', Carbon::tomorrow())
+                       ->orderBy('date', 'asc')
+                       ->get();
+
+        return view('festivals', compact('events'));
+    }
+
+    public function films(){
+        $events = Event::where('category', 'films')
+                       ->whereDate('date', '>=', Carbon::tomorrow())
+                       ->orderBy('date', 'asc')
+                       ->get();
+
+        return view('films', compact('events'));
+    }
 }
