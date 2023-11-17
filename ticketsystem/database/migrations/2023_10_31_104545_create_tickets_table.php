d176f04d417996a5ce29c0aa4faeba5d39fc1d80
RubenKorse <121227805+RubenKorse@users.noreply.github.com>
2023-11-17 10:49:51 +0100

    added a new colum to the ticket table
    This colum is there so we can check if a ticket is used already so that 20 people can all use 1 ticket

--- ticketsystem/database/migrations/2023_10_31_104545_create_tickets_table.php
+++ ticketsystem/database/migrations/2023_10_31_104545_create_tickets_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('type');
             $table->decimal('price');
+            $table->bolean('is_used');
             $table->foreignid('reservation_id')->references('id')->on('reservations');
             $table->timestamps();
         });
